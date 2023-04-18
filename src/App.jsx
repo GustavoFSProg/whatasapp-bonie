c2a51210e3a734362e5b41e661cee635bfb7c8e1
Gustavo <gustavo.prog40@gmail.com>
2023-04-18 16:33:07 -0300

    sicebar chatlist

--- src/App.jsx
+++ src/App.jsx
@@ -6,52 +6,49 @@ import avatar from './assets/avatar.png'
 
 import DonutLargeIcon from '@mui/icons-material/DonutLarge'
 import ChatIcon from '@mui/icons-material/Chat'
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import MoreVertIcon from '@mui/icons-material/MoreVert'
 
 function App() {
   return (
     <div className="app-window">
       <div className="sidebar">
-        <header >
+        <header>
           <img className="header-avatar" src={avatar} alt="avatar" />
 
-          <div style={{ display: 'flex', flexDirection: 'column', height: '1.5rem', width: '7.8rem' }}>
-
-
-          <div className="header-buttons">
-
-            <div className="header-btn">
-              <DonutLargeIcon size="28" color="blue" />
-              <ChatIcon size="28" color="blue" />
-              <MoreVertIcon size="28" color="blue" />
-
+          <div
+            style={{ display: 'flex', flexDirection: 'column', height: '1.5rem', width: '7.8rem' }}
+          >
+            <div className="header-buttons">
+              <div className="header-btn">
+                <DonutLargeIcon size="28" color="blue" />
+                <ChatIcon size="28" color="blue" />
+                <MoreVertIcon size="28" color="blue" />
               </div>
-
-
-
             </div>
-
-
-
-
           </div>
-
-
-
-
         </header>
 
         <div className="search-input">
           <input type="text" />
+        </div>
+
+
+        <div className="chatlist">
+          <img className="header-avatar" src={avatar} alt="avatar" />
+          <div className='chat-list-container'>
+
+            <span style={{ marginLeft: '-18px', fontFamily: 'Arial', fontSize: '17px' }}>
+              Fulano
+            </span>
+
+            <span style={{ marginLeft: '0px', fontFamily: 'Arial', fontSize: '14px' }}>
+              Mensagem
+            </span>
+          </div>
+
+
 
         </div>
-        <br />
-        <br />
-        
-        <div className="search">Search</div>
-
-        <div className="chatlist">chatlist</div>
-
       </div>
 
       <div className="contentarea">contentarea</div>
