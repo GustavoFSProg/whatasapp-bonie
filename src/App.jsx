c2dc725d1c2702d6b413756aeeacf8738c044a91
Gustavo <gustavo.prog40@gmail.com>
2023-04-24 11:23:07 -0300

    backup

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,42 @@ import EmojiPicker from 'emoji-picker-react'
 
 function App() {
   const [clicked, setClicked] = useState(false)
+  const [listen, setListen] = useState(false)
+  const [list, setList] = useState([])
+  const [chatlist, setChatList] = useState(false)
+
+  let recognition = null
+
+  let SpechRecgonition = window.SpechRecgonition || window.webkitSpechRecognition
+
+  if (SpechRecgonition !== undefined) {
+    recognition = new SpechRecgonition()
+  }
+
+  function handleChatList() {
+    setChatList(true)
+
+  }
+
+  function handleMicClick() {
+
+    if (recognition !== null) {
+      recognition.onstart = () => {
+        setListen(true)
+      }
+
+      recognition.onend = () => {
+        setListen(false)
+      }
+
+      recognition.onresult = (e) => {
+        setText(e.results[0][0].transcript)
+      }
+
+      recognition.start()
+
+    }
+  }
 
   function handleEmoji() {
     setClicked(true)
@@ -63,7 +99,12 @@ function App() {
           </button>
         </div>
 
-        <div className="chatlist">
+        <button onClick={() => setChatList(true)}> setar
+        </button>
+
+
+        <div className="chatlist" >
+
           <img className="header-avatar" src={avatar} alt="avatar" />
           <div className="chat-list-container">
             <span style={{ marginLeft: '-18px', fontFamily: 'Arial', fontSize: '17px' }}>
@@ -81,13 +122,35 @@ function App() {
               Mensagem
             </span>
           </div>
-          {/* <a href={() =>
-            <EmojiPicker />
-          }> */}
-        </div>
-      </div>
 
-      <div className="contentarea">
+
+        </div>
+
+
+
+        <div className="contentarea">
+          {chatlist === true ? <>
+
+            <div className="body"
+              style={{
+                width: '100%',
+                height: '90vh',
+                background: '#fff7e6',
+              }}
+            >
+              <h1>Porra!!</h1>
+              {list.map((item, key) => {
+                <MessageItem
+                  key={key}
+                  data={item}
+                />
+
+              })}
+
+            </div>
+
+          </> : <p>asdf</p>
+          }
         <div
           style={{
             display: 'flex',
@@ -119,32 +182,18 @@ function App() {
           >
             Fulano
           </p>
-          <div style={{
-            display: 'flex',
-            alignItems: 'center',
-            marginLeft: '590px'
-
-          }}>
-
-            <SearchIcon
-
-              style={{ display: 'flex', marginRight: '20px', fontSize: '29px' }}
-
-            />
-            <MoreVertIcon color="blue"
-              style={{ display: 'flex', fontSize: '29px' }}
-            />
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                marginLeft: '590px',
+              }}
+            >
+              <SearchIcon style={{ display: 'flex', marginRight: '20px', fontSize: '29px' }} />
+              <MoreVertIcon color="blue" style={{ display: 'flex', fontSize: '29px' }} />
+            </div>
           </div>
 
-        </div>
-        <div
-          style={{
-            width: '100%',
-            height: '90vh',
-
-            background: '#fff7e6',
-          }}
-        ></div>
 
         <div
           style={{
@@ -157,12 +206,12 @@ function App() {
             alignItems: 'center',
           }}
         >
-          <div style={{ display: 'flex', alignItems: 'center', }}>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
             {clicked === true ? (
               <div
                 style={{
                   marginTop: '-499px',
-                  width: 'auto'
+                    width: 'auto',
                 }}
               >
                 {/* width="55rem" */}
@@ -183,7 +232,8 @@ function App() {
                   onClick={() => setClicked(true)}
                   style={{
                     marginLeft: '18px',
-                    cursor: 'pointer', color: '#737373'
+                    cursor: 'pointer',
+                    color: '#737373',
                   }}
                   fontSize="large"
                 />
@@ -193,47 +243,49 @@ function App() {
                     color: '#737373',
                     fontSize: '27px',
                   }}
-                />
-
-                </>
-
+                    />
+                  </>
             )}
           </div>
-          <div style={{
-            display: 'flex',
-            marginRight: '80px',
-          }}>
-
+            <div
+              style={{
+                display: 'flex',
+                marginRight: '80px',
+              }}
+            >
             <input
               type="text"
-              placeholder="Mensagem"
+                placeholder="Digite a Mensagem"
               style={{
-                width: '42.9rem', fontSize: '15px',
-                height: '2.4rem', paddingLeft: '15px',
+                width: '42.9rem',
+                fontSize: '15px',
+                height: '2.4rem',
+                paddingLeft: '15px',
+              }}
+            />
+            </div>
+        </div>
+          <div onClick={handleMicClick}>
+            <MicIcon
+              style={{
+                fontSize: '29px',
+                display: 'flex',
+                marginLeft: '808px',
+                marginTop: '-48px',
+                color: listen === true ? '#126ece' : '#919191',
               }}
             />
           </div>
 
-        </div>
-        <MicIcon
-          style={{
-            fontSize: '29px',
-            display: 'flex',
-            // marginRight: '28px',
-            marginLeft: '808px',
-            marginTop: '-48px',
-            color: '#737373',
-          }}
-        />
-        <div>
 
+          <div>
           {/* <TextRotationDownIcon style={{
             fontSize: '29px', display: 'flex', marginRight: '28px',
             color: '#737373'
           }} /> */}
-
         </div>
         <br />
+      </div>
       </div>
     </div>
   )
