0d5483f25f3be9ba719815b74e09224932459e82
GFSprog40 <gustavo.prog40@gmail.com>
2023-10-05 18:10:47 -0300

    feitoria

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,27 +1,9 @@
 // import api from "../api"
 import { useAuthState, useSignInWithGoogle } from 'react-firebase-hooks/auth'
-import { useCollectionData } from 'react-firebase-hooks/firestore'
 import { auth, databaseApp } from '../firebaseConfig'
-import { collection, query, orderBy, addDoc, limit } from 'firebase/firestore'
-import { useState } from 'react'
 
 function Login({ onReceive }) {
   const [SignInWithGoogle] = useSignInWithGoogle(auth)
-
-  const messageRef = collection(databaseApp, 'usuarios')
-  const QueryMessages = query(messageRef, orderBy('id', 'asc'))
-  const [my_messages] = useCollectionData(QueryMessages, { idField: 'name' })
-
-  const [user] = useAuthState(auth)
-
-  async function HandleLogin() {
-    // console.log(`mensagem: ${my_messages}`)
-
-    await SignInWithGoogle()
-
-    const { photoURL, displayName } = auth.currentUser
-    console.log(`NAME: ${displayName}`)
-  }
 
   return (
     <div
@@ -39,7 +21,7 @@ function Login({ onReceive }) {
           background: 'white',
           width: '40%'
         }}
-        onClick={() => HandleLogin()}
+        onClick={() => SignInWithGoogle()}
       >
         {' '}
         Entrar com o Google
